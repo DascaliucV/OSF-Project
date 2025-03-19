475bf11d3a5f172d2a8dc18c3832aa3765d5cd21
DascaliucV <dascaliuc_valentyn@icloud.com>
2025-03-19 19:22:05 +0200

    Module 05-- Fix for Discount Callout
    I fixed the discount percentage issues.

--- cartridges/app_training/cartridge/scripts/helpers/productHelpers.js
+++ cartridges/app_training/cartridge/scripts/helpers/productHelpers.js
@@ -6,7 +6,7 @@ function calculatePercentageOff(standardPrice, salePrice) {
     var discountPercentage = 0; // Initialize with a default value
 
     // Ensure both prices are valid numbers (greater than 0) before calculating
-    if (standardPrice > 0 && salePrice > 0 && salePrice < standardPrice) {
+    if ((standardPrice > 0) && (salePrice > 0) && (salePrice < standardPrice)) {
         discountPercentage =
             ((standardPrice - salePrice) / standardPrice) * 100;
         discountPercentage = Math.round(discountPercentage);
